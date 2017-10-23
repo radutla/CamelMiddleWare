7d7f56d5f0e87cab2fcafd6b2eac7fc735b924d2
pranavsharmacs <33032330+pranavsharmacs@users.noreply.github.com>
2017-10-23 17:16:56 +0200

    Add files via upload

---
+++ CamelmiddlewareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.proximus.bootcamel.she.camelmiddleware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelmiddlewareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
